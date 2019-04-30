4797ec9833c60dba87b5042ab6616fd0f25dd9fe
Lukas Becker <Lukas.Becker@stud.hshl.de>
2019-04-30 16:58:52 +0200

    Feld

--- Snakes and ladders/src/objects/Feld.java
+++ Snakes and ladders/src/objects/Feld.java
@@ -2,4 +2,16 @@ package objects;
 
 public class Feld {
 
+// Feste Werte für die Größe des Felds, sowie Schlangen und Leitern
+	private final int ROWS = 10;
+	private final int COLS = 10;
+	private final int NUM_Snakes = 8;
+	private final int NUM_Ladders = 8;
+
+//Feld Variablen
+	private int[][] gameboard;
+	private int[][] snakes;
+	private int[][] ladders;
+	
+
 }
