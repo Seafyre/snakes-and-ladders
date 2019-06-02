caeb635c0132142d4526afb8b288d66ee023eebc
GeeHopper <stevengee@t-online.de>
2019-06-02 15:42:27 +0200

    small improvements

--- Snakes and ladders/src/objects/main.java
+++ Snakes and ladders/src/objects/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game game = new Game(800, 600, 100);
+		Game game = new Game(1000, 1000, 100);
 		game.run();
 	}
 
